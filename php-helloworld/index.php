a52de5469748326e70e0f245023b885e02ebd35e
shalu2311 <42956133+shalu2311@users.noreply.github.com>
2022-07-05 10:44:39 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
-print "Hello, shalinisssskkkkk! php version is " . PHP_VERSION . "\n";
+print "Hello, shalinisssskkkkk buahahahaha! php version is " . PHP_VERSION . "\n";
 print "Good Day. wassup yo!! ";
 print "This is fun!! ";
 ?>
